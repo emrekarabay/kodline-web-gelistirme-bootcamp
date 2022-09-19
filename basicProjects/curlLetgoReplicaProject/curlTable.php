84b6ecf80b874074b0c40639a311f7e78535a6f1
Your Name <karabayemre10@gmail.com>
2022-09-19 15:11:14 +0300

    curlLetgoReplicaProject eklendi

---
+++ basicProjects/curlLetgoReplicaProject/curlTable.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href=""><img src="https://statics.olx.com.tr/external/base/img/letgo/logo.svg"></a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="./curlTable.php">Ana Sayfa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Araba</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Telefon</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Elektronik</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Ev Eşyaları</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="row">
+    <?php
+        require'dbConnectPhp.php';
+
+    $sorguUsers = $conn->query(" select * from caldiklarim ");
+
+    $usersListele = $sorguUsers -> fetchall();
+
+    foreach ($usersListele as $user) {
+
+    ?>
+
+                <div class="col-4 mt-3 ">
+                    <div class="card mx-auto" style="width: 18rem;">
+                        <img src="<?php echo $user["url"] ?>" class="card-img-top" alt="..." height="200px" width="200px">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $user['baslik']; ?></h5>
+                            <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                            <a href="#" class="btn btn-primary">Sepete Ekle</a>
+                        </div>
+                    </div>
+                </div>
+
+    <?php
+    } ?>
+</div>
+
+
+<?php ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ basicProjects/curlLetgoReplicaProject/yeniCurl.php
@@ -0,0 +1,30 @@
+<?php
+require "dbConnectPhp.php";
+
+$link = baglan("https://www.letgo.com/", 0);
+preg_match_all('@data-aut-id="itemTitle">(.*?)</span>@', $link, $urlArray);
+preg_match_all('@><img src="https://apollo(.*?)" alt@', $link, $imgArray);
+
+function baglan($a)
+{
+
+    $c = curl_init();
+    curl_setopt($c, CURLOPT_URL, $a);
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($c, CURLOPT_TIMEOUT, 10);
+
+    $kaydet = curl_exec($c);
+    curl_close($c);
+    return $kaydet;
+}
+
+for ($i=0; $i<count($urlArray) ;$i++) {
+    $sorgu=$conn->prepare("INSERT INTO caldiklarim (baslik,url) values(?,?)");
+    $x = "https://apollo" . $imgArray[1][$i];
+    $sorgu->execute([$urlArray[1][$i],$x]);
+}
+
+header('Location: ./curlTable.php');
+
