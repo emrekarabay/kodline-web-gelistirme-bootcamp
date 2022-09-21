744a74580e66211999bc70bfe5b33f870e73ae3f
Your Name <karabayemre10@gmail.com>
2022-09-21 12:22:41 +0300

    15.gün ve tr.php eklendi

---
+++ 15.gün/tr.php
@@ -0,0 +1,11 @@
+
+<?php
+
+$dil["anasayfa"] = "Ana Sayfa";
+$dil["iletisim"] = "İletişim";
+$dil["karsilama"] = "İletişim sayfasına hoşgeldiniz";
+
+//setcookie("kullanici","süleyman emre",time()+(86400*30));
+//setcookie("sifre","123456",time()+(86400*30));
+
+?>
