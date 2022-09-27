5f5fea3b8eb65358274c6921319541948a7bc79e
emrekarabay <karabayemre10@gmail.com>
2022-09-27 17:22:35 +0300

    clear.php eklendi

---
+++ 18.gün/clear.php
@@ -0,0 +1,20 @@
+<?php
+/*
+$clearVariable = "<br><span>Sül\\eyman <a href='./index.php'>Emre</a>  Karabay</span>";
+
+echo htmlspecialchars($clearVariable) . "<br>";
+echo strip_tags($clearVariable);
+*/
+
+$variable = "karabayemre10@gmail.com";
+
+echo $variable . "<br>";
+
+echo trim($variable,"@gmail.com");
+/*
+echo trim($variable) . "<br>";;
+
+echo ltrim($variable) . "<br>";;
+
+ echo rtrim($variable);
+*/
