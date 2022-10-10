a551748a44044498aa1f7e70cfa177538d95b46e
Süleyman Emre Karabay <karabayemre10@gmail.com>
2022-10-10 14:51:35 +0300

    25.gun eklendi

---
+++ 25.gün/ornek1/htmlPratice.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Emre Karaabay</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<!--<span style="background-color: red;width: 20%">Süleyman</span>-->
+<!--<h1 style="background-color: blue;width: 20%;display: inline">h1 Emre Karabay</h1>-->
+<!--<h2 style="background-color: yellow;width: 20%;display: inline">h2 Emre Karabay</h2>-->
+<!--<h3 style="background-color: red;width: 20%;display: inline">h3 Emre Karabay</h3>-->
+<!--<h4 style="background-color: blue;width: 20%;display: inline">h4 Emre Karabay</h4>-->
+<!--<h5 style="background-color: yellow;width: 20%;display: inline">h5 Emre Karabay</h5>-->
+<!--<h6 style="background-color: red;width: 20%;display: inline">h6 Emre Karabay</h6>-->
+<!--<img src="https://picsum.photos/300/200" alt="image">-->
+
+<!--<div style="background-color: aqua;" >-->
+<!--    <div style="background-color: bisque;display: inline-block;width: 500px;">-->
+<!--        <span style="display: inline"> 1.div</span>-->
+<!--    </div>-->
+<!--    <div style="background-color: orange;display: inline-block;width: 500px;">-->
+<!--        <span style="display: inline"> 2.div</span>-->
+<!--    </div>-->
+<!--    <div style="background-color: pink;display: inline-block;width: 500px;">-->
+<!--        <span style="display: inline"> 3.div</span>-->
+<!--    </div>-->
+<!---->
+<!--</div>-->
+
+<ul id="enDisID">
+    <li class="liler">Ana Sayfa</li>
+    <li class="liler">Kurslar</li>
+    <ol>
+        <li class="liler">Mobil Uygulama</li>
+        <li class="liler">Web geliştirme<ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>javaScript</li>
+            </ul></li>
+    </ol>
+    <li class="liler">İletişim</li>
+</ul>
+
+</body>
+</html>
