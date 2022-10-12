ea5fbcf217ca608be7f3994be9818972c52683cb
Süleyman Emre Karabay <karabayemre10@gmail.com>
2022-10-12 16:53:03 +0300

    27.gun eklendi

---
+++ 27.gün/selector.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Emre Karabay</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<!--    <ul class="ul">-->
+<!--        <li>Ana Sayfa</li>-->
+<!--        <li class="classyeni">Hakkımızda</li>-->
+<!--        <li>İletişim</li>-->
+<!--    </ul>-->
+
+
+    <div class="disdiv sagustdiv">2</div>
+    <div class="disdiv solustdiv">1</div>
+    <div class="disdiv sagaltdiv">4</div>
+    <div class="disdiv solaltdiv">3</div>
+    <div class="disdiv ortadiv">5</div>
+
+</body>
+</html>
