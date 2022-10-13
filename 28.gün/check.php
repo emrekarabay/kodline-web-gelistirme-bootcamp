ba369ff5dda23c33df0bb570a006129ed0bea8df
Süleyman Emre Karabay <karabayemre10@gmail.com>
2022-10-13 17:17:15 +0300

    28.gun eklendi

---
+++ 28.gün/check.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Emre Karabay</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+<div class="btnDiv">
+    <label for="checkbox" class="btn">Check i Tıkla</label>
+</div>
+
+    <input type="checkbox" name="" class="" id="checkbox">
+
+
+   <div class="disdiv">
+        <label for="checkbox" class="icon"><i class="fa fa-brands fa-times" aria-hidden="true"></i></label>
+        <input type="text" placeholder="Kullanıcı Adı:" class="input2">
+        <input type="password" placeholder="Parola:" class="input2">
+        <button class="btn2">Giriş</button>
+    </div>
+
+</body>
+</html>
+
